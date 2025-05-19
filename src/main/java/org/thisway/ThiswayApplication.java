97e09c2dcbf992327f27754904ba14cf7b3f5b5b
Jeongyounghyeon <snorlax1106@naver.com>
2025-05-19 15:39:13 +0900

    chore: init Spring project

---
+++ src/main/java/org/thisway/ThiswayApplication.java
@@ -0,0 +1,13 @@
+package org.thisway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiswayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThiswayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/thisway/ThiswayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thisway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiswayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
