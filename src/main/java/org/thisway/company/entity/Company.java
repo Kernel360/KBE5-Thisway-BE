02169415f61b2415ef8f1877a39cda90a4d30913
devyoon <wideskyinme@gmail.com>
2025-05-21 16:59:32 +0900

    feat: Company 엔티티 정의

--- src/main/java/org/thisway/company/entity/Company.java
+++ src/main/java/org/thisway/company/entity/Company.java
@@ -7,4 +7,43 @@ import org.thisway.common.BaseEntity;
 @Entity
 @Getter
 public class Company extends BaseEntity {
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String crn;
+
+    @Column(nullable = false)
+    private String contact;
+
+    @Column(nullable = false)
+    private String addrRoad;
+
+    @Column(nullable = false)
+    private String addrDetail;
+
+    @Column(nullable = false)
+    private String memo;
+
+    @Column(nullable = false)
+    private Integer gpsCycle;
+
+    @Builder
+    public Company(
+            String name,
+            String crn,
+            String contact,
+            String addrRoad,
+            String addrDetail,
+            String memo,
+            Integer gpsCycle
+    ) {
+        this.name = name;
+        this.crn = crn;
+        this.contact = contact;
+        this.addrRoad = addrRoad;
+        this.addrDetail = addrDetail;
+        this.memo = memo;
+        this.gpsCycle = gpsCycle;
+    }
 }
