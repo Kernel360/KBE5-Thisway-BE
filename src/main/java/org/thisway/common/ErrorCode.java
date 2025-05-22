76b528f9b01ce192245be38ea6fd7b09f35d9411
roomie00 <roomie@shin-ahreum.local>
2025-05-22 18:16:08 +0900

    feat: #9 이메일 인증 코드 발송 API 개발

--- src/main/java/org/thisway/common/ErrorCode.java
+++ src/main/java/org/thisway/common/ErrorCode.java
@@ -16,9 +16,11 @@ public enum ErrorCode {
     COMPANY_NOT_FOUND(HttpStatus.NOT_FOUND, "회사 정보를 찾을 수 없습니다."),
     COMPANY_ALREADY_EXIST(HttpStatus.CONFLICT, "이미 존재하는 회사입니다."),
 
+    // 멤버
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 정보를 찾을 수 없습니다."),
     MEMBER_ALREADY_EXIST_BY_EMAIL(HttpStatus.CONFLICT, "이미 등록된 이메일입니다."),
     MEMBER_INVALID_PHONE_NUMBER(HttpStatus.BAD_REQUEST, "유효하지 않은 핸드폰 번호입니다."),
+    INVALID_VERIFY_CODE(HttpStatus.BAD_REQUEST, "잘못된 인증 코드입니다.")
     ;
 
     private final HttpStatus status;
