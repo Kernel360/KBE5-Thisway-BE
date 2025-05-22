23a4d1eaa6bd636ad1aec3c5cd4c090636d75e03
Jeongyounghyeon <snorlax1106@naver.com>
2025-05-22 15:22:32 +0900

    feat: 잘못된 `MemberRepository` Member Id 타입 수정
    `Integer` -> `Long`

--- src/main/java/org/thisway/member/repository/MemberRepository.java
+++ src/main/java/org/thisway/member/repository/MemberRepository.java
@@ -3,5 +3,5 @@ package org.thisway.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.thisway.member.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Integer> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
