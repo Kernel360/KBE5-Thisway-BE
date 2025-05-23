2adc9c4786d2a45b26bdc614a2e68eed9cbe67df
roomie00 <roomie@shin-ahreum.local>
2025-05-23 15:32:12 +0900

    feat: #9 controller 통합 테스트

---
+++ src/test/java/org/thisway/auth/controller/AuthControllerTest.java
@@ -0,0 +1,86 @@
+package org.thisway.auth.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.thisway.auth.dto.request.SendVerifyCodeRequest;
+import org.thisway.common.ApiResponse;
+import org.thisway.member.entity.Member;
+import org.thisway.member.repository.MemberRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@RequiredArgsConstructor
+public class AuthControllerTest {
+
+    private final MockMvc mockMvc;
+    private final ObjectMapper objectMapper;
+
+    private final MemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp() {
+        memberRepository.deleteAll();
+        memberRepository.save(Member.builder()
+                .name("홍길동")
+                .email("abc@example.com")
+                .password("password1234")
+                .phone("010-1234-5678")
+                .memo("메모메모")
+                .build());
+    }
+
+    @Test
+    @DisplayName("이메일 인증 코드 전송에 성공했을 때, ok 응답을 한다.")
+    void givenValidEmail_whenSendVerifyCode_thenReturnOkStatus() throws Exception {
+        SendVerifyCodeRequest request = new SendVerifyCodeRequest("abc@example.com");
+
+        MvcResult mvcResult = mockMvc.perform(
+                post("/api/auth/verify-code")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                )
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        String responseBody = mvcResult.getResponse().getContentAsString();
+        ApiResponse<?> response = objectMapper.readValue(responseBody, ApiResponse.class);
+        assertThat(response.status()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    @DisplayName("이메일 인증 코드 요청 시 존재하지 않는 이메일을 입력하면, not_found 응답을 한다.")
+    void givenInvalidEmail_whenSendVerifyCode_thenReturnNotFoundStatus() throws Exception {
+        SendVerifyCodeRequest request = new SendVerifyCodeRequest("1234@example.com");
+
+        MvcResult mvcResult = mockMvc.perform(
+                post("/api/auth/verify-code")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                )
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        String responseBody = mvcResult.getResponse().getContentAsString();
+        ApiResponse<?> response = objectMapper.readValue(responseBody, ApiResponse.class);
+        assertThat(response.status()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    }
+
+}
