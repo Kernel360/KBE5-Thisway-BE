86db4fdfc6eaff8cd1b145d6a5289f2c2cbbe6b8
devyoon <wideskyinme@gmail.com>
2025-05-26 12:24:07 +0900

    refactor: #31 스케줄링 어노테이션 추가

--- src/main/java/org/thisway/ThiswayApplication.java
+++ src/main/java/org/thisway/ThiswayApplication.java
@@ -2,8 +2,10 @@ package org.thisway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ThiswayApplication {
 
     public static void main(String[] args) {
