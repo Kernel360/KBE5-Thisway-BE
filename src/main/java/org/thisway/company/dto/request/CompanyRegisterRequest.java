987879be0daeb6e703ebd7ccc7283c65e180f38d
Jeongyounghyeon <snorlax1106@naver.com>
2025-05-27 10:02:51 +0900

    refactor: #42 `CompanyRegisterRequest` 사용되지 않는 `from` 메서드 삭제

--- src/main/java/org/thisway/company/dto/request/CompanyRegisterRequest.java
+++ src/main/java/org/thisway/company/dto/request/CompanyRegisterRequest.java
@@ -3,8 +3,6 @@ package org.thisway.company.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import org.thisway.company.dto.response.CompanyResponse;
-import org.thisway.company.entity.Company;
 
 public record CompanyRegisterRequest(
         @NotBlank
@@ -28,16 +26,4 @@ public record CompanyRegisterRequest(
         @Positive
         Integer gpsCycle
 ) {
-
-    public static CompanyResponse from(Company company) {
-        return new CompanyResponse(
-                company.getName(),
-                company.getCrn(),
-                company.getContact(),
-                company.getAddrRoad(),
-                company.getAddrDetail(),
-                company.getMemo(),
-                company.getGpsCycle()
-        );
-    }
 }
