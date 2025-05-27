c963ac3f0b0e9e48b8cfe81fd7e3c9d680861011
Jeongyounghyeon <snorlax1106@naver.com>
2025-05-27 10:06:48 +0900

    refactor: #42 의미없는 애너테이션 삭제

--- src/main/java/org/thisway/company/repository/CompanyRepository.java
+++ src/main/java/org/thisway/company/repository/CompanyRepository.java
@@ -1,6 +1,5 @@
 package org.thisway.company.repository;
 
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +9,5 @@ public interface CompanyRepository extends JpaRepository<Company, Long> {
 
     Page<Company> findAllByActiveTrue(Pageable pageable);
 
-    Boolean existsByCrn(@NotBlank String crn);
+    Boolean existsByCrn(String crn);
 }
