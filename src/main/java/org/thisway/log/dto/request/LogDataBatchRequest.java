e9aa74a1e712ab3433c2b0a1b06469b8d9740433
devyoon <wideskyinme@gmail.com>
2025-05-27 12:30:35 +0900

    refactor: #31 규격서에 따른 구조 변경으로 인한 기존 Log클래스들 삭제
    - LogBatchRequest.java
    - LogDataEntry.java
    - LogDataSaveService.java
    - LogDataSaveServiceTest.java

--- src/main/java/org/thisway/log/dto/request/LogDataBatchRequest.java
+++
@@ -1,9 +0,0 @@
-package org.thisway.log.dto.request;
-
-import java.util.List;
-
-public record LogDataBatchRequest(
-        Long vehicleId,
-        Long mdn,
-        List<LogDataEntry> entries
-) {}
