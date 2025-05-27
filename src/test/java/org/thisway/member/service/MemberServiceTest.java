6d19eb0b2db28b9df8334a9fe5e3a476393f85f1
Jeongyounghyeon <snorlax1106@naver.com>
2025-05-27 14:40:25 +0900

    refactor: #41 Member 관련 테스트 메서드명 한글화

--- src/test/java/org/thisway/member/service/MemberServiceTest.java
+++ src/test/java/org/thisway/member/service/MemberServiceTest.java
@@ -37,7 +37,7 @@ class MemberServiceTest {
 
     @Test
     @DisplayName("멤버가 정상적으로 조회된다.")
-    void givenValidMemberId_whenGetMemberDetail_thenSuccessfulGetDetail() {
+    void 멤버_조회_테스트_성공() {
         // given
         Member member = MemberFixture.createMember();
 
@@ -53,7 +53,7 @@ class MemberServiceTest {
 
     @Test
     @DisplayName("없는 멤버를 조회하려 하면 member not found exception이 발생한다.")
-    void givenInvalidMemberId_whenGetMemberDetail_thenThrowNotFoundException() {
+    void 멤버_조회_테스트_없는_멤버() {
         // given
         Member member = MemberFixture.createMember();
 
@@ -69,7 +69,8 @@ class MemberServiceTest {
     }
 
     @Test
-    void 멤버가_페이징_정보에_맞게_정상적으로_조회된다() {
+    @DisplayName("멤버가 페이징 정보에 맞게 정상적으로 조회된다")
+    void 멤버_페이징_조회_테스트_성공() {
         // given
         List<Member> members = List.of(
                 MemberFixture.createMember(),
@@ -90,7 +91,7 @@ class MemberServiceTest {
 
     @Test
     @DisplayName("멤버 등록이 정상적으로 등록된다.")
-    void givenValidRequest_whenRegisterMember_thenDoesNotThrowAnyException() {
+    void 멤버_등록_테스트_성공() {
         // given
         MemberRegisterRequest request = MemberFixture.createMemberRegisterRequest();
 
@@ -109,8 +110,8 @@ class MemberServiceTest {
     }
 
     @Test
-    @DisplayName("멤버 등록이 정상적으로 등록된다.")
-    void 멤버_등록시_존재하는_이메일의_회원일_경우_예외가_발생한다() {
+    @DisplayName("멤버_등록시_존재하는_이메일의_회원일_경우_예외가_발생한다")
+    void 멤버_등록_테스트_존재하는_이메일() {
         // given
         String email = "hong@example.com";
         MemberRegisterRequest request = MemberFixture.createMemberRegisterRequestWithEmail(email);
@@ -124,7 +125,7 @@ class MemberServiceTest {
 
     @Test
     @DisplayName("멤버가 정상적으로 삭제된다.")
-    void givenValidMemberId_whenDeleteMember_thenSuccessfulDelete() {
+    void 멤버_삭제_테스트_성공() {
         // given
         Member member = MemberFixture.createMember();
 
@@ -141,7 +142,7 @@ class MemberServiceTest {
 
     @Test
     @DisplayName("없는 멤버를 삭제하려 하면 member not found exception이 발생한다.")
-    void givenNotFoundMemberId_whenDeleteMember_thenThrowNotFoundException() {
+    void 멤버_삭제_테스트_없는_멤버() {
         // when & then
         CustomException e = assertThrows(CustomException.class, () -> memberService.deleteMember(1L));
 
