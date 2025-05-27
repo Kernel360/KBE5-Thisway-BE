bd9cabe5ce6d3e9269f88780935a228ae8bb2c75
Jeongyounghyeon <snorlax1106@naver.com>
2025-05-27 16:18:32 +0900

    refactor: #44 예외 발생시 로그 발생 로직 추가
    성##[Allowed <type>]###############################

--- src/main/java/org/thisway/common/GlobalExceptionHandler.java
+++ src/main/java/org/thisway/common/GlobalExceptionHandler.java
@@ -1,19 +1,29 @@
 package org.thisway.common;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ApiErrorResponse> handleRuntimeException() {
+    public ResponseEntity<ApiErrorResponse> handleRuntimeException(RuntimeException e) {
+        log.error(e.getMessage(), e);
         return ApiErrorResponse.of(ErrorCode.SERVER_ERROR);
     }
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ApiErrorResponse> handleCustomException(CustomException e) {
+        HttpStatus status = e.getErrorCode().getStatus();
+        if (status.is5xxServerError()) {
+            log.error(e.getMessage(), e);
+        } else {
+            log.info(e.getMessage(), e);
+        }
         return ApiErrorResponse.of(e.getErrorCode());
     }
 }
