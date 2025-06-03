51c9cfed600204d883a5018887006255e1d714d0
devyoon <wideskyinme@gmail.com>
2025-06-04 08:47:07 +0900

    feat: #73 EmulatorRepository findByVehicleId 메서드추가

--- src/main/java/org/thisway/emulator/repository/EmulatorRepository.java
+++ src/main/java/org/thisway/emulator/repository/EmulatorRepository.java
@@ -6,4 +6,6 @@ import org.thisway.emulator.entity.Emulator;
 
 public interface EmulatorRepository extends JpaRepository<Emulator, Long> {
     Optional<Emulator> findByMdn(String mdn);
+
+    Optional<Emulator> findByVehicleId(Long vehicleId);
 }
