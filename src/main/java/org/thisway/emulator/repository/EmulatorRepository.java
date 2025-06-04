ca692565f6c2a899589827d5a1cf2e5289e7cd2f
devyoon <wideskyinme@gmail.com>
2025-06-04 11:13:39 +0900

    feat: #73 패키지 순환참조 방지를 위한 EmulatorFinder 상속

--- src/main/java/org/thisway/emulator/repository/EmulatorRepository.java
+++ src/main/java/org/thisway/emulator/repository/EmulatorRepository.java
@@ -3,9 +3,12 @@ package org.thisway.emulator.repository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.thisway.emulator.entity.Emulator;
+import org.thisway.vehicle.api.EmulatorFinder;
 
-public interface EmulatorRepository extends JpaRepository<Emulator, Long> {
+public interface EmulatorRepository extends JpaRepository<Emulator, Long>, EmulatorFinder {
+    @Override
     Optional<Emulator> findByMdn(String mdn);
 
+    @Override
     Optional<Emulator> findByVehicleId(Long vehicleId);
 }
