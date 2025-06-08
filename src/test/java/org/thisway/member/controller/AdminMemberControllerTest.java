a05738e36010bebb7893c4f3edc68024b6337509
Jeongyounghyeon <snorlax1106@naver.com>
2025-06-08 16:45:03 +0900

    test: #97 ADMIN API Controller 사용자 수정, 삭제 테스트 추가

--- src/test/java/org/thisway/member/controller/AdminMemberControllerTest.java
+++ src/test/java/org/thisway/member/controller/AdminMemberControllerTest.java
@@ -4,8 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,6 +27,7 @@ import org.thisway.common.PageInfo;
 import org.thisway.member.dto.MemberDto;
 import org.thisway.member.dto.MembersDto;
 import org.thisway.member.dto.request.AdminMemberRegisterRequest;
+import org.thisway.member.dto.request.AdminMemberUpdateRequest;
 import org.thisway.member.dto.response.MemberResponse;
 import org.thisway.member.dto.response.MembersResponse;
 import org.thisway.member.entity.MemberRole;
@@ -126,6 +129,38 @@ class AdminMemberControllerTest {
                         .content(requestBody)
                 )
                 .andExpect(status().isCreated())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("멤버를 수정할 수 있다.")
+    @WithMockUser(authorities = "ADMIN")
+    void 멤버_수정_테스트() throws Exception {
+        // given
+        AdminMemberUpdateRequest request = new AdminMemberUpdateRequest(
+                "name",
+                "email",
+                "phone",
+                "memo"
+        );
+        String requestBody = objectMapper.writeValueAsString(request);
+
+        // when & then
+        mockMvc.perform(put("/api/admin/members/" + 1L)
+                        .contentType(APPLICATION_JSON)
+                        .content(requestBody)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("멤버를 삭제할 수 있다.")
+    @WithMockUser(authorities = "ADMIN")
+    void 멤버_삭제_테스트() throws Exception {
+        // when & then
+        mockMvc.perform(delete("/api/admin/members/" + 1L))
+                .andExpect(status().isNoContent())
                 .andDo(print());
     }
 }
