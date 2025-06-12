8d77f8d49d1946dd5b4e2474977316b78ed5301d
Shin Dong Jun <tlsehdwns147@naver.com>
2025-06-12 13:30:48 +0900

    feat: #125 VehicleModel 전체 조회 기능

--- src/main/java/org/thisway/vehicle/service/VehicleModelService.java
+++ src/main/java/org/thisway/vehicle/service/VehicleModelService.java
@@ -1,6 +1,9 @@
 package org.thisway.vehicle.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.thisway.common.CustomException;
@@ -9,6 +12,7 @@ import org.thisway.member.entity.Member;
 import org.thisway.member.entity.MemberRole;
 import org.thisway.security.service.SecurityService;
 import org.thisway.vehicle.dto.request.VehicleModelCreateRequest;
+import org.thisway.vehicle.dto.response.VehicleModelsResponse;
 import org.thisway.vehicle.entity.VehicleModel;
 import org.thisway.vehicle.repository.VehicleModelRepository;
 
@@ -20,6 +24,11 @@ public class VehicleModelService {
   private final VehicleModelRepository vehicleModelRepository;
   private final SecurityService securityService;
 
+  private final static int MAX_PAGE_SIZE = 20;
+  private final List<String> ALLOWED_SORT_PROPERTIES = List.of(
+      "id", "manufacturer", "model", "modelYear"
+  );
+
   public void registerVehicleModel(VehicleModelCreateRequest request) {
 
     Member member = getCurrentMember();
@@ -27,7 +36,26 @@ public class VehicleModelService {
     validateDuplicateModel(request);
     VehicleModel vehicleModel = request.toEntity();
     vehicleModelRepository.save(vehicleModel);
+  }
 
+  public VehicleModelsResponse getVehicleModels(Pageable pageable){
+    validatePageable(pageable);
+    Member member = getCurrentMember();
+    Page<VehicleModel> vehicleModels = vehicleModelRepository.findAllByActiveTrue(pageable);
+    return VehicleModelsResponse.from(vehicleModels);
+  }
+
+
+  private void validatePageable(Pageable pageable) {
+    if(pageable.getPageSize() > MAX_PAGE_SIZE){
+      throw new CustomException(ErrorCode.PAGE_INVALID_PAGE_SIZE);
+    }
+
+    pageable.getSort().forEach(order -> {
+      if(!ALLOWED_SORT_PROPERTIES.contains(order.getProperty())){
+        throw new CustomException(ErrorCode.PAGE_INVALID_SORT_PROPERTY);
+      }
+    });
   }
 
   /**
