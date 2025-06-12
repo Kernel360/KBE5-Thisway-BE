3652af76db271b676f9137fc07c6e57f52dcf5f9
devyoon <wideskyinme@gmail.com>
2025-06-12 14:54:07 +0900

    feat: #119 에뮬레이터 서비스레이어 dto 정의

---
+++ src/main/java/org/thisway/emulator/service/dto/input/EmulatorUpdateInput.java
@@ -0,0 +1,25 @@
+package org.thisway.emulator.service.dto.input;
+
+import lombok.Builder;
+
+@Builder
+public record EmulatorUpdateInput(
+        Long id,
+        String mdn,
+        Long vehicleId,
+        String terminalId,
+        Integer manufactureId,
+        Integer packetVersion,
+        Integer deviceId,
+        String deviceFirmwareVersion
+) {
+    public boolean isEmpty() {
+        return mdn == null &&
+                vehicleId == null &&
+                terminalId == null &&
+                manufactureId == null &&
+                packetVersion == null &&
+                deviceId == null &&
+                deviceFirmwareVersion == null;
+    }
+}
