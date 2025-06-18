84350cfc73831c4730ab189111e6aff3df465e25
Jeongyounghyeon <snorlax1106@naver.com>
2025-06-18 17:07:20 +0900

    fix: #161 없는 RabbitMQ에 연결 시도 버그 해결
    rabbitmq mode일때만 Bean 생성하도록 변경경

--- src/main/java/org/thisway/log/consumer/GpsLogConsumer.java
+++ src/main/java/org/thisway/log/consumer/GpsLogConsumer.java
@@ -3,6 +3,7 @@ package org.thisway.log.consumer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.thisway.config.ampq.RabbitMQConfig;
 import org.thisway.log.dto.request.gpsLog.GpsLogRequest;
@@ -10,6 +11,7 @@ import org.thisway.log.service.GpsLogSaveService;
 
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(name = "gps-log-collect-mode", havingValue = "rabbitmq")
 @Slf4j
 public class GpsLogConsumer {
 
