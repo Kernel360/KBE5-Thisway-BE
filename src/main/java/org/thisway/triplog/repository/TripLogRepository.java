66505e298d79c8270399641a3917041d7d8bf0fe
roomie00 <ocar1115.shin@gmail.com>
2025-06-19 14:21:09 +0900

    feat: #158 시동 위치 통계 조회 API 생성

--- src/main/java/org/thisway/triplog/repository/TripLogRepository.java
+++ src/main/java/org/thisway/triplog/repository/TripLogRepository.java
@@ -1,8 +1,25 @@
 package org.thisway.triplog.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.thisway.triplog.dto.response.TripLocationStats;
 import org.thisway.triplog.entity.TripLog;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface TripLogRepository extends JpaRepository<TripLog, Long> {
 
+    @Query("SELECT new org.thisway.triplog.dto.response.TripLocationStats(t.onAddr, COUNT(t)) " +
+            "FROM TripLog t " +
+            "WHERE t.vehicle.company.id = :companyId " +
+            "AND t.startTime >= :from AND t.startTime <= :to " +
+            "GROUP BY t.onAddr"
+    )
+    List<TripLocationStats> countGroupedByOnAddr(
+            @Param("companyId")Long companyId,
+            @Param("from")LocalDateTime from,
+            @Param("to")LocalDateTime to
+    );
 }
---
+++ src/main/java/org/thisway/triplog/service/StatisticService.java
@@ -0,0 +1,22 @@
+package org.thisway.triplog.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.thisway.security.service.SecurityService;
+import org.thisway.triplog.dto.response.TripLocationStats;
+import org.thisway.triplog.repository.TripLogRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StatisticService {
+    private final TripLogRepository tripLogRepository;
+
+    public List<TripLocationStats> getStartLocationStatBetweenDates(Long companyId, LocalDateTime startTime, LocalDateTime endTime) {
+        return tripLogRepository.countGroupedByOnAddr(companyId, startTime, endTime);
+    }
+}
