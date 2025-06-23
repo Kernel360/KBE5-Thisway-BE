0b9c23bfd0ccc974932bad9441788e2b71022135
Jeongyounghyeon <snorlax1106@naver.com>
2025-06-23 12:16:07 +0900

    feat: #167 요청, 응답 로그 남기기
    - password, newPassword, token 등의 정보는 Filtered 된 정보로 로그가 남도록 설정

---
+++ src/main/java/org/thisway/logging/filter/filter/LoggingFilter.java
@@ -0,0 +1,47 @@
+package org.thisway.logging.filter.filter;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+import org.thisway.logging.filter.utils.LogSanitizer;
+
+@Component
+@Slf4j
+public class LoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+        MDC.put("traceId", UUID.randomUUID().toString());
+
+        try {
+            filterChain.doFilter(requestWrapper, responseWrapper);
+
+            String requestBody = new String(requestWrapper.getContentAsByteArray(), request.getCharacterEncoding());
+            String responseBody = new String(responseWrapper.getContentAsByteArray(), response.getCharacterEncoding());
+
+            log.info("Request Body: {}", LogSanitizer.sanitize(requestBody));
+            log.info("Response Body: {}", LogSanitizer.sanitize(responseBody));
+        } finally {
+            responseWrapper.copyBodyToResponse();
+            MDC.clear();
+        }
+    }
+}
