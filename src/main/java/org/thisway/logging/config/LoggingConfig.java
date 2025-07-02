1688731287f88af7f04e8ca4ab5b572758f0699d
Clsz <45088680+ClszK@users.noreply.github.com>
2025-07-02 18:25:41 +0900

    refactor: #203 deprecate 된 메서드 변경

--- src/main/java/org/thisway/logging/config/LoggingConfig.java
+++ src/main/java/org/thisway/logging/config/LoggingConfig.java
@@ -3,6 +3,7 @@ package org.thisway.logging.config;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.introspect.AnnotationIntrospectorPair;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
@@ -19,24 +20,24 @@ public class LoggingConfig {
 
     @Bean(name = "maskingObjectMapper")
     public ObjectMapper maskingObjectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
-
         JavaTimeModule javaTimeModule = new JavaTimeModule();
-        javaTimeModule.addSerializer(LocalDate.class,
+
+        javaTimeModule.addSerializer(
+                LocalDate.class,
                 new LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE));
-        javaTimeModule.addSerializer(LocalDateTime.class,
+        javaTimeModule.addSerializer(
+                LocalDateTime.class,
                 new LocalDateTimeSerializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-        objectMapper.registerModule(javaTimeModule);
 
-        objectMapper.setAnnotationIntrospector(
-                AnnotationIntrospectorPair.pair(
+        JsonMapper mapper = JsonMapper.builder()
+                .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+                .addModule(javaTimeModule)
+                .annotationIntrospector(AnnotationIntrospectorPair.pair(
                         new MaskingIntroSpector(),
-                        objectMapper.getSerializationConfig().getAnnotationIntrospector()
-                )
-        );
+                        JsonMapper.builder().build()
+                                .getSerializationConfig().getAnnotationIntrospector()))
+                .build();
 
-        return objectMapper;
+        return mapper;
     }
 }
