f0142894c703b5b4968f1bab1af8e8a950f156a2
Clsz <45088680+ClszK@users.noreply.github.com>
2025-07-04 16:06:43 +0900

    refactor: #203 마스킹 임시 제외

--- src/main/java/org/thisway/member/controller/dto/request/PasswordChangeRequest.java
+++ src/main/java/org/thisway/member/controller/dto/request/PasswordChangeRequest.java
@@ -1,13 +1,11 @@
 package org.thisway.member.controller.dto.request;
 
-import org.thisway.logging.masking.MaskingData;
-
 import jakarta.validation.constraints.NotBlank;
 
 public record PasswordChangeRequest(
     @NotBlank String email,
     @NotBlank String code,
-    @MaskingData @NotBlank String newPassword
+    @NotBlank String newPassword
 ) {
 
 }
