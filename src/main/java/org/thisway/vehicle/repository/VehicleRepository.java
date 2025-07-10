49e18568248c9507b917dd1d20eddc15d3bbe590
Shin Dong Jun <tlsehdwns147@naver.com>
2025-07-10 17:15:06 +0900

    feat: #220 전체 차량 조회 시 내림차순 정렬 적용

--- src/main/java/org/thisway/vehicle/repository/VehicleRepository.java
+++ src/main/java/org/thisway/vehicle/repository/VehicleRepository.java
@@ -1,20 +1,16 @@
 package org.thisway.vehicle.repository;
 
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.thisway.company.entity.Company;
 import org.thisway.vehicle.entity.Vehicle;
-
-import java.util.Optional;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long>, VehicleQueryRepository {
 
     Optional<Vehicle> findByIdAndActiveTrue(Long id);
 
     boolean existsByCarNumberAndActiveTrue(String carNumber);
-
-    Page<Vehicle> findAllByCompanyAndActiveTrue(Company company, Pageable pageable);
 
     Page<Vehicle> findAllByCompanyIdAndPowerOnIsAndActiveTrue(long companyId, boolean powerOn, Pageable pageable);
 
