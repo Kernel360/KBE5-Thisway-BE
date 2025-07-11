b121c235b9fc0dd51d5f0148fe3fdd9a63b92131
Shin Dong Jun <tlsehdwns147@naver.com>
2025-07-11 16:17:39 +0900

    feat: #228 StatisticsResponse(record) -> StatisticsResponse(class)로 변경

--- src/main/java/org/thisway/company/statistics/interfaces/StatisticResponse.java
+++ src/main/java/org/thisway/company/statistics/interfaces/StatisticResponse.java
@@ -1,84 +1,88 @@
 package org.thisway.company.statistics.interfaces;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import lombok.Value;
 import org.thisway.company.statistics.StatisticConstants;
 import org.thisway.company.statistics.domain.Statistics;
 import org.thisway.vehicle.triplog.domain.TripLocationStats;
 
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.List;
+@Value
+public class StatisticResponse {
 
-public record StatisticResponse(
-        Long companyId,
-        String date,
-        Integer powerOnCount,
-        Double averageDailyPowerCount,
-        Integer totalDrivingTime,
-        Integer peakHour,
-        Double peakHourRate,
-        Integer lowHour,
-        Double lowHourRate,
-        Double averageOperationRate,
-        List<Double> hours,
-        List<TripLocationStats> locationStats
-) {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+   Long companyId;
+   String date;
+   Integer powerOnCount;
+   Double averageDailyPowerCount;
+   Integer totalDrivingTime;
+   Integer peakHour;
+   Double peakHourRate;
+   Integer lowHour;
+   Double lowHourRate;
+   Double averageOperationRate;
+   List<Double> hours;
+   List<TripLocationStats> locationStats;
 
-    public static StatisticResponse from(Statistics statistics, List<TripLocationStats> locationStats) {
-        // Statistics 엔티티의 getHourlyRatesArray() 메서드 활용
-        List<Double> hours = Arrays.asList(statistics.getHourlyRatesArray());
+   private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-        // peakHour와 lowHour의 실제 퍼센트 값 계산
-        Double peakHourRate = getSafeHourlyRate(statistics.getPeakHour(), hours);
-        Double lowHourRate = getSafeHourlyRate(statistics.getLowHour(), hours);
+   public static StatisticResponse from(Statistics statistics, List<TripLocationStats> locationStats){
+     List<Double> hours = Arrays.asList(statistics.getHourlyRatesArray());
 
-        return new StatisticResponse(
-                statistics.getCompany().getId(),
-                statistics.getDate().format(FORMATTER),
-                statistics.getPowerOnCount(),
-                statistics.getAverageDailyPowerCount(),
-                statistics.getTotalDrivingTime(),
-                statistics.getPeakHour(),
-                peakHourRate,
-                statistics.getLowHour(),
-                lowHourRate,
-                statistics.getAverageOperationRate(),
-                hours,
-                locationStats
-        );
-    }
+     Double peakHourRate = getSafeHourlyRate(statistics.getPeakHour(), hours);
+     Double lowHourRate = getSafeHourlyRate(statistics.getLowHour(), hours);
 
-    /**
-     * 배열 기반 생성자 (집계된 통계용)
-     */
-    public static StatisticResponse fromAggregatedData(
-            Long companyId, String dateRange, Integer powerOnCount, Double averageDailyPowerCount,
-            Integer totalDrivingTime, Integer peakHour, Integer lowHour, Double averageOperationRate,
-            List<Double> hourlyRates, List<TripLocationStats> locationStats) {
+     return new StatisticResponse(
+         statistics.getCompany().getId(),
+         statistics.getDate().format(formatter),
+         statistics.getPowerOnCount(),
+         statistics.getAverageDailyPowerCount(),
+         statistics.getTotalDrivingTime(),
+         statistics.getPeakHour(),
+         peakHourRate,
+         statistics.getLowHour(),
+         lowHourRate,
+         statistics.getAverageOperationRate(),
+         hours,
+         locationStats
+     );
+   }
 
-        Double peakHourRate = getSafeHourlyRate(peakHour, hourlyRates);
-        Double lowHourRate = getSafeHourlyRate(lowHour, hourlyRates);
+   public static StatisticResponse fromAggregatedData(
+       Long companyId,
+       String dateRange,
+       Integer powerOnCount,
+       Double averageDailyPowerCount,
+       Integer totalDrivingTime,
+       Integer peakHour,
+       Integer lowHour,
+       Double averageOperationRate,
+       List<Double> hourlyRates,
+       List<TripLocationStats> locationStats){
 
-        return new StatisticResponse(
-                companyId,
-                dateRange,
-                powerOnCount,
-                averageDailyPowerCount,
-                totalDrivingTime,
-                peakHour,
-                peakHourRate,
-                lowHour,
-                lowHourRate,
-                averageOperationRate,
-                hourlyRates,
-                locationStats
-        );
-    }
+     Double peakHourRate = getSafeHourlyRate(peakHour, hourlyRates);
+     Double lowHourRate = getSafeHourlyRate(lowHour, hourlyRates);
 
-    private static Double getSafeHourlyRate(Integer hour, List<Double> hourlyRates) {
-        if (hour == null || hour < 0 || hour >= hourlyRates.size()) {
-            return StatisticConstants.DEFAULT_HOURLY_RATE;
-        }
-        return hourlyRates.get(hour);
-    }
+     return new StatisticResponse(
+          companyId,
+          dateRange,
+          powerOnCount,
+          averageDailyPowerCount,
+          totalDrivingTime,
+          peakHour,
+          peakHourRate,
+          lowHour,
+          lowHourRate,
+          averageOperationRate,
+          hourlyRates,
+          locationStats
+      );
+   }
+
+   private static Double getSafeHourlyRate(Integer hour, List<Double> hourlyRates){
+     if(hour == null || hour < 0 || hour >= hourlyRates.size()){
+       return StatisticConstants.DEFAULT_HOURLY_RATE;
+     }
+     return hourlyRates.get(hour);
+   }
 }
